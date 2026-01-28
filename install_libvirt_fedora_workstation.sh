97f69d5c644bda4ec24a6d6051228aa168b270a7
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2026-01-28 08:31:14 -0300

    Add additional packages for libvirt installation

--- install_libvirt_fedora_workstation.sh
+++ install_libvirt_fedora_workstation.sh
@@ -3,6 +3,8 @@ flatpak uninstall org.virt_manager.virt-manager
 #Instala todos os pacotes referentes ao libvirt
 dnf search libvirt | grep -E "(^ libvirt|^ virt)" | grep -viE "(java|ruby|perl|vbox)" | cut -f1 -d"." | sort | uniq | xargs -n 100 sudo dnf -y install
 
+sudo dnf -y install virt-top libguestfs-tools guestfs-tools
+
 #Habilita e executa o serviço
 sudo service libvirtd start
 
@@ -11,4 +13,3 @@ sudo service libvirtd enable
 service libvirtd status
 
 echo "Adicionar o usuário $USER no grupo LibVirt"
-
