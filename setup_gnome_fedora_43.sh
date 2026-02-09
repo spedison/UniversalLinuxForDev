20ff4d136364f83cdde8b9534a4d120665400869
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2026-02-09 09:32:06 -0300

    Install GNOME extensions and tweaks for Fedora
    Added installation of additional GNOME extensions and tools.

--- setup_gnome_fedora_43.sh
+++ setup_gnome_fedora_43.sh
@@ -1,9 +1,18 @@
 
-sudo dnf install gnome-tweaks
-sudo dnf install gnome-extensions-app
+#TODO Filtrar para executar somente em Fedora
+sudo dnf install -y \
+  gnome-shell-extension-appindicator \
+  gnome-extensions-app \
+  gnome-tweaks \
+  gnome-shell-extension-just-perfection
   
 dnf search   cursor-theme 2> /dev/null | grep -E "^ [a-z]" | cut -f 1 -d "." | xargs -n 10 sudo dnf -y install
 dnf search   icon-theme 2> /dev/null | grep -E "^ [a-z]" | cut -f 1 -d "." | xargs -n 10 sudo dnf -y install
+
+curl  https://extensions.gnome.org/extension-data/just-perfection-desktopjust-perfection.v36.shell-extension.zip   -o /tmp/shell.zip
+
+gnome-extensions install /tmp/shell.zip
+
   
 gsettings set org.gnome.desktop.interface cursor-theme 'breeze_cursors'
 gsettings set org.gnome.desktop.interface icon-theme 'Yaru-purple'
