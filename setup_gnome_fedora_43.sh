d2339dd71e377628185e3384076b1965d75c3456
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2026-02-09 10:25:21 -0300

    Install and enable GNOME extensions for customization

--- setup_gnome_fedora_43.sh
+++ setup_gnome_fedora_43.sh
@@ -10,9 +10,11 @@ dnf search   cursor-theme 2> /dev/null | grep -E "^ [a-z]" | cut -f 1 -d "." | x
 dnf search   icon-theme 2> /dev/null | grep -E "^ [a-z]" | cut -f 1 -d "." | xargs -n 10 sudo dnf -y install
 
 curl  https://extensions.gnome.org/extension-data/just-perfection-desktopjust-perfection.v36.shell-extension.zip   -o /tmp/shell.zip
-
 gnome-extensions install /tmp/shell.zip
+gnome-extensions  enable just-perfection-desktop@just-perfection
 
+curl https://extensions.gnome.org/extension-data/blur-my-shellaunetx.v70.shell-extension.zip -o /tmp/blur.zip
+gnome-extensions install /tmp/shell.zip
   
 gsettings set org.gnome.desktop.interface cursor-theme 'breeze_cursors'
 gsettings set org.gnome.desktop.interface icon-theme 'Yaru-purple'
